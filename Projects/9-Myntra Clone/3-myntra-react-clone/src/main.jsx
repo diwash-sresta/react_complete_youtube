beb896a20f94961e59aeb99fc8213ac69bbf47d1
Diwash <diwashshrestha108@gmail.com>
2025-07-05 13:40:12 +0545

    Myntra react clone

---
+++ Projects/9-Myntra Clone/3-myntra-react-clone/src/main.jsx
@@ -0,0 +1,28 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./routes/App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Bag from "./routes/Bag.jsx";
+import Home from "./routes/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> /*, loader: postLoader*/ },
+      {
+        path: "/bag",
+        element: <Bag />,
+        /*action: createPostAction,*/
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
