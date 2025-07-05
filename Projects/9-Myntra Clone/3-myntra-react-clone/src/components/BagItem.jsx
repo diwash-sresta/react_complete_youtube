ce6aa5f9552ca9c35102af21632cb590999365e1
Diwash <diwashshrestha108@gmail.com>
2025-07-05 23:16:55 +0545

    connected backend data with frontend

--- Projects/9-Myntra Clone/3-myntra-react-clone/src/components/BagItem.jsx
+++ Projects/9-Myntra Clone/3-myntra-react-clone/src/components/BagItem.jsx
@@ -26,7 +26,7 @@ const BagItem = ({ item }) => {
 
       <div
         className="remove-from-cart"
-        onclick={() => console.log("Item removed form bag")}
+        onClick={() => console.log("Item removed form bag")}
       >
         X
       </div>
